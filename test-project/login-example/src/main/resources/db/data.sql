08cd94fdfe2e0ff06e5922b85e126e77ab47ae05
clean17 <piw940317@gmail.com>
2023-02-02 11:53:41 +0900

    인터셉터 테스트 세팅 완료

---
+++ test-project/login-example/src/main/resources/db/data.sql
@@ -0,0 +1,4 @@
+insert into user_tb (username, password, email, enabled) values ('ssar','1234','ssar@nate.com', 'on');
+insert into user_tb (username, password, email, enabled) values ('love1','1234','ssar@nate.com', 'on');
+
+commit;
