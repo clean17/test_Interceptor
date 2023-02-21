4f874dc22caf9d277ad29f79441fceeb95423676
clean17 <piw940317@gmail.com>
2023-02-21 10:42:24 +0900

    test: 회원가입 insert쿼리 테스트 완료

--- test-project/login-example/src/main/java/com/example/loginexample/dto/UserReq.java
+++ test-project/login-example/src/main/java/com/example/loginexample/dto/UserReq.java
@@ -1,9 +1,11 @@
 package com.example.loginexample.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 public class UserReq {
+    @AllArgsConstructor
     @Getter
     @Setter
     public static class UserJoinDto{
@@ -17,6 +19,7 @@ public class UserReq {
         private String username;
         private String password;
     }
+    
     @Getter
     @Setter
     public static class UserUpdateReqDto{
